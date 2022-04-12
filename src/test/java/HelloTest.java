e826c026fafb4c12d8f60e43868e44b75e9cfd35
orhanozsalman <orhanozsalman@hotmail.com>
2022-04-12 22:14:03 +0200

    Added more feature

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -1,6 +1,11 @@
 public class HelloTest {
+
     public static void main(String[] args) {
+
         System.out.println("Hello Test");
         System.out.println("Developing Hello Feature");
+        System.out.println("Local Repo has already published");
+        System.out.println("Doing More work");
+
     }
 }
